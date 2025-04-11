198c32920dc78acd68af56611517d0a9a6edd0d4
sofia0760 <camelojimenezsofia20@gmail.com>
2025-04-11 14:19:28 -0500

    primer commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function modalVideo(){
+
+    let selector =document.getElementById("modal-1")
+
+    selector.classList.toggle("active")
+
+
+}
+function menuMobile(){
+    let selector = document.getElementById("cont-menu-mobile")
+    selector.classList.toggle("active")
+}
